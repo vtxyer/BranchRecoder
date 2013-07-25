7f20dd96e8b4fe3c3eddcf2e00234136b168b173
VTxyer <vtxyer@gmail.com>
2013-07-25 14:13:08 -0400

    add change files

---
+++ TOOLS/Recorder_tools/branch_recorder.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <csignal>
+
+#include "branch_recorder.h"
+#include <set>
+using namespace std;
+
+
+int main(int argc, char *argv[])  
+{
+	int host_domID;
+	int tmp = 0;
+	unsigned long guest_cr3, host_cr3;
+	void *page_table_space;
+	struct debug_store *ds;
+	struct Bts_record *bts;
+	struct pollfd poll_fd;
+	unsigned long bts_size_order;
+	int port, rc;
+	FILE *fout;
+	Node_val* container[MON_NUM] = {NULL};
+	//signal(SIGINT, end_process);
+
+
+	/*Init data_base*/
+	for(int k=0; k<MON_NUM; k++){
+		data_base[k].ds = NULL;
+		data_base[k].bts = NULL;
+	}
+
+
+	printf("PID: %d\n", getpid());
+	printf("Input host domID: ");
+	fscanf(stdin, "%d", &host_domID);
+	printf("Input guest domID: ");
+	fscanf(stdin, "%d", &guest_domID);
+	printf("Input guest cr3: ");
+	fscanf(stdin, "%lx", &guest_cr3);
+	printf("Host pid %d, Input host cr3: ", getpid());
+	fscanf(stdin, "%lx", &host_cr3);
+	printf("BTS size[4K]: ");
+	fscanf(stdin, "%lu", &bts_size_order);
+
+    fd = open("/proc/xen/privcmd", O_RDWR);  
+    if (fd < 0) {   
+        perror("open");  
+        exit(1);  
+    }   
+	fout = fopen("Trace_data", "w");
+
+
+	/*Alloc space for page table*/
+	page_table_space = memalign(PAGE_SIZE, PAGE_SIZE);
+	memset(page_table_space, 1, PAGE_SIZE);
+	set_ept(fd, guest_domID, host_domID, host_cr3, (unsigned long)page_table_space);
+	set_guest_page_table(fd, guest_domID, guest_cr3);
+
+	vpmu_data = (struct vpmu_data *)memalign(PAGE_SIZE, PAGE_SIZE);
+	if(vpmu_data == NULL){
+		printf("vpmu_data alloc error\n");
+		return -1;
+	}
+	//memset(vpmu_data, 0x99, sizeof(*vpmu_data));
+	vpmu_data->host_domID = host_domID;	
+	init_vpmu_data(guest_domID, vpmu_data, bts_size_order, fd, host_cr3);
+
+
+	//Event channel init	
+	xch = xc_evtchn_open(NULL, 0);
+	if(xch == NULL){
+		fprintf(stderr, "xch alloc error\n");
+		exit(1);
+	}
+	xc_evtchn_bind_virq(xch, 20);
+	poll_fd.fd = xc_evtchn_fd(xch);
+	poll_fd.events = POLLIN | POLLERR;
+	set_event_chn(fd, host_domID);	
+
+
+	rc = 0;
+	/*Waiting for VIRQ*/
+	while(1){
+		rc = poll(&poll_fd, 1, -1);
+		if(rc == -1){
+			fprintf(stderr, "poll exitted with an error");
+			return -1;
+		}
+		else if(rc == 1){
+			port = xc_evtchn_pending(xch);
+			if (port == -1) {
+				perror("failed to read port from evtchn");
+				exit(EXIT_FAILURE);
+			}
+//			printf("port %d\n", port);
+			rc = xc_evtchn_unmask(xch, port);
+			if (rc == -1) {
+				perror("failed to write port to evtchn");
+				return -1;
+			}		
+			/*proceed data*/
+			for(int k=0; k<MON_NUM; k++){
+				if(data_base[k].ds != NULL && data_base[k].bts != NULL){
+					ds = data_base[k].ds;
+					bts = data_base[k].bts;
+					if(ds->bts_interrupt_threshold == ds->bts_index){
+						go_through_bts(bts, ds->bts_buffer_base, ds->bts_index, fout);
+						printf("ds->bts_interrupt_threshold:%lx ds->bts_index: %lx  ds->bts_buffer_base:%lx ptr:%d\n", 
+								data_base[0].ds->bts_interrupt_threshold, data_base[0].ds->bts_index, data_base[0].ds->bts_buffer_base, k);
+						printf("ds->bts_interrupt_threshold:%lx ds->bts_index: %lx  ds->bts_buffer_base:%lx ptr:%d\n", 
+								data_base[1].ds->bts_interrupt_threshold, data_base[1].ds->bts_index, data_base[1].ds->bts_buffer_base, k);
+						ds->bts_index = ds->bts_buffer_base;
+					}
+				}
+			}
+		}
+
+	}
+
+	return 0;
+}
+
+
---
+++ TOOLS/Recorder_tools/branch_recorder.h
@@ -0,0 +1,236 @@
+extern "C"{
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <errno.h>  
+#include <sys/ioctl.h>  
+#include <sys/types.h>  
+#include <fcntl.h>  
+#include <string.h>  
+#include <xenctrl.h>  
+#include <xen/sys/privcmd.h> 
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <xenstore.h>
+//#include <xen/hvm/save.h>
+#include <setjmp.h>
+#include <malloc.h>
+#include <signal.h>
+#include <poll.h>
+}
+#include <map>
+#include <fstream>
+using namespace std;
+
+typedef unsigned long u64;
+#define PAGE_SIZE 			4096
+#define BTS_RECORD_SIZE		24
+#define MAX_PEBS_EVENTS      8
+#define MON_NUM				24
+
+
+/*Global variable*/
+int fd, guest_domID;
+xc_interface *xch;
+
+
+struct vpmu_data *vpmu_data;
+struct debug_store {
+	u64	bts_buffer_base;
+	u64	bts_index;
+	u64	bts_absolute_maximum;
+	u64	bts_interrupt_threshold;
+	u64	pebs_buffer_base;
+	u64	pebs_index;
+	u64	pebs_absolute_maximum;
+	u64	pebs_interrupt_threshold;
+	u64	pebs_event_reset[MAX_PEBS_EVENTS];
+};
+struct vpmu_data{
+	unsigned long host_domID; 
+	unsigned long host_cr3; 
+	unsigned long host_ds_addr[2];
+	unsigned long host_bts_base[2];
+	unsigned long bts_size_order;
+};
+struct Bts_record {
+    u64 from;
+    u64 to;
+    u64 flags;
+};
+struct Node_val{
+	struct debug_store *ds;
+	struct Bts_record *bts;
+	int waring;
+};
+struct Node_val data_base[MON_NUM];
+
+
+extern "C"
+{
+	int get_bts_flag(int fd, int domID){
+		int ret;
+		unsigned long bts_flag[2];
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 0, domID, 0, 0, 0}
+		};  
+		ret = ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);
+		
+		if(ret<0){
+			perror("get bts flag error\n");
+			return ret;
+		}
+		else{
+			return ret;
+		}
+	}
+	int sent_vpmu_data(int fd, int domID, struct vpmu_data *vpmu_data){
+		int ret;
+/*		printf("host_domID:%d host_cr3:%lx vpmu_data:%lx\n",
+			(vpmu_data->host_domID), (vpmu_data->host_cr3), (unsigned long)vpmu_data
+		);*/
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 1, domID, (vpmu_data->host_domID), (vpmu_data->host_cr3), (unsigned long)vpmu_data}
+		};  
+		ret = ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);
+		
+		return ret;
+	}
+	void clean_vpmu(int fd, int _guest_domID){
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 6, _guest_domID, 0, 0, 0}
+		};  
+		ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);		
+	}
+	void set_ept(int fd, int _guest_domID, int _host_domID, unsigned long _host_cr3, unsigned long _va){
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 5, _guest_domID, _host_domID, _host_cr3, _va}
+		};  
+		ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);		
+	}
+	void set_guest_page_table(int fd, int _guest_domID, unsigned long _guest_cr3){
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 9, _guest_domID, _guest_cr3, 0, 0}
+		};  
+		ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);		
+	}
+	void start_monitoring(int fd, int _guest_domID, int op, unsigned long target_cr3){
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 2, _guest_domID, op, target_cr3, 0}
+		};  
+		ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);		
+	}
+	void stop_monitoring(int fd, int _guest_domID){
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 3, _guest_domID, 0, 0, 0}
+		};  
+		ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);		
+	}
+	void set_event_chn(int fd, int _host_domID){
+		privcmd_hypercall_t hyper1 = { 
+			__HYPERVISOR_vt_op, 
+			{ 4, _host_domID, 0, 0, 0}
+		};  
+		ioctl(fd, IOCTL_PRIVCMD_HYPERCALL, &hyper1);		
+	}
+}                                    
+
+
+void end_process(int errno){
+	clean_vpmu(fd, guest_domID);
+	xc_interface_close(xch);
+	close(fd);
+    exit(errno);
+}
+
+unsigned long pre_from;
+void go_through_bts(Bts_record *bts, unsigned long start, unsigned long end, FILE *fd){
+	unsigned long ptr = start;
+	Bts_record *bts_buffer = bts;
+	while(ptr < end){
+//		fprintf(fd, "From:%lx To:%lx               ptr:%lx end:%lx\n", bts_buffer->from, bts_buffer->to, ptr, end);
+		if(bts_buffer->from != pre_from){
+			fprintf(fd, "From:%lx To:%lx\n", bts_buffer->from, bts_buffer->to);
+		}
+		pre_from = bts_buffer->from;
+		bts_buffer += 1;
+		ptr += BTS_RECORD_SIZE;
+	}
+}
+
+int init_vpmu_data(int domID, struct vpmu_data *vpmu_data, unsigned long bts_size_order, int fd, unsigned long host_cr3)
+{
+	void *bts_buffer;
+	int bts_enable;
+	unsigned long ret;
+	unsigned long tmp;	
+	int now_data_base_ptr = 0;
+	struct debug_store *ds;
+
+	bts_enable = get_bts_flag(fd, domID);
+	if(bts_enable < 0){
+		return -1;
+	}
+
+//	if(bts_enable == 0){	
+	if(1){	
+		vpmu_data->bts_size_order = bts_size_order;
+
+		vpmu_data->host_cr3 = host_cr3;
+
+		/*alloc debug store register*/
+		for(int k=0; k<2; k++){
+			//each vpmu have two bts info
+			data_base[now_data_base_ptr].ds = (struct debug_store *)memalign(PAGE_SIZE, PAGE_SIZE);	
+			ds = data_base[now_data_base_ptr].ds;
+			if(ds == NULL){
+				printf("<VT> ds alloc error\n");
+				return -1;
+			}
+			memset(ds, 0, sizeof(*ds));
+			vpmu_data->host_ds_addr[k] = (unsigned long)ds; 
+
+			bts_buffer = memalign(PAGE_SIZE, bts_size_order*PAGE_SIZE);
+			if(bts_buffer == NULL)
+			{
+				printf("<VT> bts buffer alloc error\n");
+				return -1;
+			}
+			memset(bts_buffer, 1, bts_size_order*PAGE_SIZE);
+			memset(bts_buffer, 0, bts_size_order*PAGE_SIZE);
+			vpmu_data->host_bts_base[k] = (unsigned long)bts_buffer;
+			data_base[now_data_base_ptr].bts = (struct Bts_record *)bts_buffer;
+
+			/*0x123 for check in hypervisor map success or not*/
+			data_base[now_data_base_ptr].ds->bts_buffer_base = 0x123;
+
+			now_data_base_ptr++;
+		}
+
+
+		/*Clean first*/
+		clean_vpmu(fd, guest_domID);
+		/*Send msgs to hypervisor*/
+		sent_vpmu_data(fd, domID, vpmu_data);
+
+
+		bts_enable = get_bts_flag(fd, domID);
+		if(bts_enable < 0){
+			return -1;
+		}	
+		printf("<VT>debug_store init ok\n");
+		return 0;
+	}
+	else{
+		printf("<VT> Already init debug store\n");
+		return -2;
+	}
+}
+
